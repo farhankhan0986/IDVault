65e4b4247b92270b6fb21985ecfeb62a7394f4fc
Farhan Abid <farhankhan080304@gmail.com>
2026-01-11 00:08:15 +0530

    card public updated

--- app/card/[id]/PublicCardClient.js
+++ app/card/[id]/PublicCardClient.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import CardUI from "@/app/components/CardUI";
+import CardUI from "../../components/CardUI";
 
 export default function PublicCardClient({ id }) {
   const [card, setCard] = useState(null);
